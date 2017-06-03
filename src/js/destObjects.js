480e0b9ffc63b98512fc2dc10fd5709456ba5f58
Alberto Perez <albertojperezf@gmail.com>
2017-06-03 13:00:39 +0200

    ADD WEBPACK AND WEBPACK DEV SERVER FOR LOCAL DEVELOPMENT

---
+++ src/js/destObjects.js
@@ -0,0 +1,12 @@
+var character = {
+    name: 'Barry Allen',
+    pseudonym: 'The Flash',
+    metadata: {
+        age: 27,
+        gender: 'male'
+    },
+}
+
+var { name } = character
+
+console.log(name);
